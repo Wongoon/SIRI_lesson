37034ef0148afe1d1713dba8c1ab946eed41200e
Wongoon <rmsgh64@gmail.com>
2022-04-18 16:31:03 +0900

    code update

---
+++ html/align-items/javascript/align-items.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+    $('ul.tabnav_ul li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+        
+        $('ul.tabnav_ul li').removeClass('current');
+        $('.tab_content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
+
+});
